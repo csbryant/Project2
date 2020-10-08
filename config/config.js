978f0064e81137bba5f462618739a93f9dc8b461
Caren Braj <carenbraj@gmail.com>
2020-10-07 21:05:53 -0700

    added APIs

--- config/config.js
+++ config/config.js
@@ -2,15 +2,15 @@ require('dotenv').config()
 
 module.exports = {
   "development": {
-    "username": process.env.DB_username,
-    "password": process.env.DB_password,
-    "database": process.env.DB_database,
+    "username": process.env.DB_username="root",
+    "password": process.env.DB_password="Indifilm1976",
+    "database": process.env.DB_database="cheatsheet_db",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "test": {
     "username": "root",
-    "password": null,
+    "password": "Indifilm1976",
     "database": "database_test",
     "host": "127.0.0.1",
     "dialect": "mysql"
