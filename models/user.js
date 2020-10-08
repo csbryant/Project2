15a30f6c738e8c2f0e8d120f55cfee1962fdca63
Chris Bryant <chris_bryant@mac.com>
2020-10-07 21:38:38 -0700

    Added files

--- models/user.js
+++ models/user.js
@@ -16,6 +16,22 @@ module.exports = function(sequelize, DataTypes) {
     password: {
       type: DataTypes.STRING,
       allowNull: false
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    city: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    state: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    zip: {
+      type: DataTypes.STRING,
+      allowNull: false
     }
   });
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
@@ -32,4 +48,6 @@ module.exports = function(sequelize, DataTypes) {
     );
   });
   return User;
+  console.log(user);
 };
+
--- public/JS/signup.js
+++ public/JS/signup.js
@@ -3,30 +3,60 @@ $(document).ready(() => {
   const signUpForm = $("form.signup");
   const emailInput = $("input#email-input");
   const passwordInput = $("input#password-input");
+  const addressInput = $("input#inputAddress");
+  const cityInput = $("input#inputCity");
+  const stateInput = $("input#inputState");
+  const zipInput = $("input#inputZip");
 
   // When the signup button is clicked, we validate the email and password are not blank
   signUpForm.on("submit", event => {
     event.preventDefault();
     const userData = {
       email: emailInput.val().trim(),
-      password: passwordInput.val().trim()
+      password: passwordInput.val().trim(),
+      address: addressInput.val().trim(),
+      city: cityInput.val().trim(),
+      state: stateInput.val().trim(),
+      zip: zipInput.val().trim()
     };
 
-    if (!userData.email || !userData.password) {
+    if (
+      !userData.email ||
+      !userData.password ||
+      !userData.address ||
+      !userData.city ||
+      !userData.state ||
+      !userData.zip
+    ) {
       return;
     }
     // If we have an email and password, run the signUpUser function
-    signUpUser(userData.email, userData.password);
+    signUpUser(
+      userData.email,
+      userData.password,
+      userData.address,
+      userData.city,
+      userData.state,
+      userData.zip
+    );
     emailInput.val("");
     passwordInput.val("");
+    addressInput.val("");
+    cityInput.val("");
+    stateInput.val("");
+    zipInput.val("");
   });
 
   // Does a post to the signup route. If successful, we are redirected to the members page
   // Otherwise we log any errors
-  function signUpUser(email, password) {
+  function signUpUser(email, password, address, city, state, zip) {
     $.post("/api/signup", {
       email: email,
-      password: password
+      password: password,
+      address: address,
+      city: city,
+      state: state,
+      zip: zip
     })
       .then(() => {
         window.location.replace("/members");
