6285e56b1cfab2afac05570470f09f24963ce220
Oscar Ledezma <ooledezma@gmail.com>
2020-10-11 22:04:25 -0700

    Dynamically add the measures

--- public/JS/tableExample.js
+++ public/JS/tableExample.js
@@ -7,7 +7,7 @@ const queryU =
 // begin first ajax call
 $.ajax({
   method: "GET",
-  url: queryU,
+  url: queryU
 }).then(function(response) {
   const main = response.ballot_item_list.filter(function(MEASURE) {
     return MEASURE.kind_of_ballot_item === "MEASURE";
@@ -16,8 +16,10 @@ $.ajax({
     const element = main[index];
     measures.push(element);
   }
-  //   render(measures);
-  for (var index = 0; index < measures.length; index++) {
+
+  measures.forEach(render);
+
+  /*   for (var index = 0; index < measures.length; index++) {
     const measureTitle = measures[index].ballot_item_display_name;
     const measureDescription = measures[index].measure_text;
     const measureYes = measures[index].yes_vote_description;
@@ -33,9 +35,67 @@ $.ajax({
     console.log(measureDescription);
     console.log(measureYes);
     console.log(measureNo);
-  }
+  } */
 });
 
-// function render(measures) {
-//   console.log(measures);
-// }
+function render(measures) {
+  //Render card title
+  var propositionCards = $("#propositionscards");
+  var carddiv = $("<div>").addClass("card text-center");
+  propositionCards.append(carddiv);
+  var cardheaderdiv = $("<div>").addClass("card-header");
+  carddiv.append(cardheaderdiv);
+  cardheaderdiv.text(measures.ballot_item_display_name);
+
+  //Render cardbody
+  var cardbodydiv = $("<div>").addClass("card-body");
+  carddiv.append(cardbodydiv);
+  var bodypelement = $("<p>").addClass("card-text");
+  cardbodydiv.append(bodypelement);
+  bodypelement.text(measures.measure_text);
+
+  //Render Yes Body
+  var yesdiv = $("<div>").addClass("col-sm noyes");
+  cardbodydiv.append(yesdiv);
+  yesdiv.text(measures.yes_vote_description);
+
+  //Render No Body
+  var nodiv = $("<div>").addClass("col-sm noyes");
+  cardbodydiv.append(nodiv);
+  nodiv.text(measures.no_vote_description);
+
+  //Render Card Footer
+  var cardfooterdiv = $("<div>").addClass("card-footer");
+  carddiv.append(cardfooterdiv);
+  var btnddiv = $("<div>")
+    .addClass("btn-group btn-group-toggle propvote")
+    .attr("data-toggle", "buttons");
+  cardfooterdiv.append(btnddiv);
+
+  //Render Yes Button
+  var labelyes = $("<label>").addClass("btn btn-secondary");
+  btnddiv.append(labelyes);
+  labelyes.text("Yes");
+  var inputyes = $(
+    "<input type='radio' name='options' id ='option1' autocomplete='off' checked>"
+  );
+  labelyes.append(inputyes);
+
+  //Render Undecided Button
+  var labelundecided = $("<label>").addClass("btn btn-secondary active");
+  btnddiv.append(labelundecided);
+  labelundecided.text("Undecided");
+  var inputundecided = $(
+    "<input type='radio' name='options' id ='option2' autocomplete='off'>"
+  );
+  labelundecided.append(inputundecided);
+
+  //Render No Button
+  var labelno = $("<label>").addClass("btn btn-secondary");
+  btnddiv.append(labelno);
+  labelno.text("No");
+  var inputno = $(
+    "<input type='radio' name='options' id ='option3' autocomplete='off'>"
+  );
+  labelno.append(inputno);
+}
